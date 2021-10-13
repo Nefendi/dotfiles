0ded299cd87d8b8bfc8bbd34ab863261ca6ae809
Nefendi <28621696+Nefendi@users.noreply.github.com>
2021-10-13 22:03:13 +0200

    Add two more useful plugins to LunarVim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -151,6 +151,9 @@ lvim.plugins = {
   {
     "folke/tokyonight.nvim"
   },
+  {
+    "lunarvim/colorschemes"
+  },
   -- {
   --   "RRethy/nvim-base16"
   -- },
@@ -212,7 +215,31 @@ lvim.plugins = {
     "ray-x/lsp_signature.nvim",
     config = function() require"lsp_signature".on_attach() end,
     event = "BufRead"
-  }
+  },
+  {
+    "nacro90/numb.nvim",
+    event = "BufRead",
+    config = function()
+    require("numb").setup {
+      show_numbers = true, -- Enable 'number' for the window while peeking
+      show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+    }
+    end,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+      config = function()
+        require("colorizer").setup({ "*" }, {
+            RGB = true, -- #RGB hex codes
+            RRGGBB = true, -- #RRGGBB hex codes
+            RRGGBBAA = true, -- #RRGGBBAA hex codes
+            rgb_fn = true, -- CSS rgb() and rgba() functions
+            hsl_fn = true, -- CSS hsl() and hsla() functions
+            css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+            css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+    end,
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
