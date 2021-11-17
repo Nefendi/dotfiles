8e094a156988edd4273ec604a08df02b202eeef3
Nefendi <28621696+Nefendi@users.noreply.github.com>
2021-11-17 16:14:33 +0100

    Clean up and update LunarVim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,4 +1,3 @@
--- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 
@@ -71,33 +70,9 @@ vim.g.symbols_outline = {
     }
 }
 
--- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
+
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
-
--- require('nvim-tree').setup {
---   filters = {
---     dotfiles = true,
---   }
--- }
 
 require('telescope').setup {
   pickers = {
@@ -123,7 +98,6 @@ require('telescope').setup {
   }
 }
 
--- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Trouble",
@@ -150,74 +124,41 @@ lvim.builtin.which_key.mappings["m"] = {
   p = { "<cmd>:Glow<cr>", "Preview" },
 }
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
 
--- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
 
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
+local formatters = require "lvim.lsp.null-ls.formatters"
 
--- set a formatter if you want to override the default lsp one (if it exists)
-lvim.lang.python.formatters = {
+formatters.setup {
   {
-    exe = "black",
+    exe = "black"
   },
   {
     exe = "isort"
-  }
-}
-
-lvim.lang.cpp.formatters = {
+  },
   {
-    exe = "clang-format",
-  }
-}
-
-lvim.lang.javascript.formatters = {
+    exe = "clang-format"
+  },
   {
     exe = "prettier"
   }
 }
 
--- set an additional linter
-lvim.lang.python.linters = {
+local linters = require "lvim.lsp.null-ls.linters"
+
+linters.setup {
   {
     exe = "flake8"
   },
   {
-    exe =  "pylint"
-  },
+    exe = "pylint"
+  }
 }
 
 -- Additional Plugins
