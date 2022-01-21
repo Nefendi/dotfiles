af71209db9727de3f5f4c1ac2692d38651f628c7
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-01-21 21:55:12 +0100

    Add a workaround for clangd

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -248,6 +248,11 @@ end
 
 lspconfig.emmet_ls.setup({ capabilities = capabilities })
 
+-- clangd multiple different client encodings workaround
+local clangd_capabilities = vim.lsp.protocol.make_client_capabilities()
+clangd_capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = clangd_capabilities })
+
 local formatters = require("lvim.lsp.null-ls.formatters")
 
 formatters.setup({
