e5ac7d199e9ddfd31f4693559614a468c2a4d189
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-03-06 11:04:08 +0100

    Revert "Disable hint in lsp_signature"
    This reverts commit b47d89e4ae5ebb194ac92adc4cf3361742670656.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -401,7 +401,7 @@ lvim.plugins = {
 	{
 		"ray-x/lsp_signature.nvim",
 		config = function()
-			require("lsp_signature").setup({ hint_enable = false })
+			require("lsp_signature").on_attach()
 		end,
 		event = "BufRead",
 	},
