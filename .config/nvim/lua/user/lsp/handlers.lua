ff7f7481099a831a2f7a9f8d8d30b5c4b19a0eeb
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-06-23 17:41:57 +0200

    Change quickfix command

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -66,7 +66,7 @@ local function lsp_keymaps(bufnr)
 	keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
 	keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 	keymap(bufnr, "n", "<leader>lh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	-- keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
