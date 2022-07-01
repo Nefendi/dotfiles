05d361d68e0caf67042dc5a2c688d1bb810badf1
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-01 22:19:53 +0200

    Change DAP breakpoint icon

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -60,7 +60,7 @@ dapui.setup({
 	windows = { indent = 1 },
 })
 
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
