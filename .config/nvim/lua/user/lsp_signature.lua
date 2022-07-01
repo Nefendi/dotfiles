cffce38e2b2c2162c173c21fb66f503d76db45d8
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-01 22:20:09 +0200

    Change lsp_signature config

--- .config/nvim/lua/user/lsp_signature.lua
+++ .config/nvim/lua/user/lsp_signature.lua
@@ -3,4 +3,11 @@ if not status_ok then
 	return
 end
 
-lsp_signature.setup()
+lsp_signature.setup({
+	floating_window = false,
+	floating_window_above_cur_line = false,
+	hint_prefix = " ",
+	hint_scheme = "Comment",
+	doc_lines = 0,
+	max_width = 120,
+})
