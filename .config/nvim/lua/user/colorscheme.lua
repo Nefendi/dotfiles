2cf88856ed2bc7ade7f1e46c7107214bb99a0ac5
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-01 17:35:56 +0200

    Change Nvim and Alacritty colorschemes to One Dark
    Neverending story...

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,9 +1,7 @@
-local colorscheme = "gruvbox"
-
-vim.g.gruvbox_contrast_dark = "hard"
-vim.g.gruvbox_sign_column = "bg0"
+local colorscheme = "onedarker"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
 end
