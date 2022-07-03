fc9a2b6f0976c8ffaa2544c8e93fdbd7286b820a
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-03 17:45:18 +0200

    Use darkplus colrscheme and modify lualine

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -1,9 +1,16 @@
--- Sources for spaces, LSP, Python env and Tresitter components: https://github.com/LunarVim/LunarVim
+-- Sources: https://github.com/LunarVim/LunarVim and https://github.com/ChristianChiarulli/nvim/blob/master/lua/user/lualine.lua
 
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
 	return
 end
+
+local status_theme_ok, theme = pcall(require, "lualine.themes.darkplus_dark")
+if not status_theme_ok then
+	return
+end
+
+local icons = require("user.icons")
 
 local hide_in_width = function()
 	return vim.fn.winwidth(0) > 70
@@ -15,10 +22,38 @@ local red = "#EC5F67"
 
 vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#E8AB53", bg = "#303030" })
 vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#D4D4D4", bg = "#303030", bold = false })
+vim.api.nvim_set_hl(0, "SLLocation", { fg = "#D4D4D4", bg = "#303030" })
+vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#808080", bg = "#252525" })
+
+local mode_color = {
+	n = "#569cd6",
+	i = "#6a9955",
+	v = "#c586c0",
+	[""] = "#c586c0",
+	V = "#c586c0",
+	c = "#4EC9B0",
+	no = "#569cd6",
+	s = "#ce9178",
+	S = "#ce9178",
+	[""] = "#ce9178",
+	ic = "#dcdcaa",
+	R = "#d16969",
+	Rv = "#d16969",
+	cv = "#569cd6",
+	ce = "#569cd6",
+	r = "#d16969",
+	rm = "#4EC9B0",
+	["r?"] = "#4EC9B0",
+	["!"] = "#4EC9B0",
+	t = "#D7BA7D",
+}
 
 local mode = {
 	function()
 		return " "
+	end,
+	color = function()
+		return { bg = mode_color[vim.fn.mode()] }
 	end,
 	padding = { left = 0, right = 0 },
 }
@@ -27,7 +62,12 @@ local diagnostics = {
 	"diagnostics",
 	sources = { "nvim_diagnostic" },
 	sections = { "error", "warn", "info", "hint" },
-	symbols = { error = " ", warn = " ", info = " ", hint = " " },
+	symbols = {
+		error = icons.diagnostics.Error .. " ",
+		warn = icons.diagnostics.Warning .. " ",
+		info = icons.diagnostics.Info .. " ",
+		hint = icons.diagnostics.Hint .. " ",
+	},
 	colored = false,
 	update_in_insert = false,
 	always_visible = true,
@@ -35,13 +75,18 @@ local diagnostics = {
 
 local diff = {
 	"diff",
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	symbols = {
+		added = icons.git.Added .. " ",
+		modified = icons.git.Modified .. " ",
+		removed = icons.git.Removed .. " ",
+	}, -- changes diff symbols
 	diff_color = {
 		added = { fg = green },
 		modified = { fg = yellow },
 		removed = { fg = red },
 	},
 	cond = hide_in_width,
+	separator = "%#SLSeparator# " .. icons.misc.ThinBar .. "%*",
 }
 
 local filetype = {
@@ -59,27 +104,29 @@ local filename = {
 local branch = {
 	"branch",
 	icons_enabled = true,
-	icon = "%#SLGitIcon#" .. "" .. "%*" .. "%#SLBranchName#",
+	icon = "%#SLGitIcon#" .. icons.git.Branch .. "%*" .. "%#SLBranchName#",
 	colored = false,
 }
 
 local spaces = {
 	function()
 		if not vim.api.nvim_buf_get_option(0, "expandtab") then
-			return "Tab size: " .. vim.api.nvim_buf_get_option(0, "tabstop")
+			return "tab size: " .. vim.api.nvim_buf_get_option(0, "tabstop")
 		end
 		local size = vim.api.nvim_buf_get_option(0, "shiftwidth")
 		if size == 0 then
 			size = vim.api.nvim_buf_get_option(0, "tabstop")
 		end
-		return "Spaces: " .. size
+		return "spaces: " .. size
 	end,
 	cond = hide_in_width,
+	-- separator = "%#SLSeparator#" .. icons.misc.RightThinBar .. "%*",
 }
 
 local fileformat = {
 	"fileformat",
-	icons_enabled = true,
+	icons_enabled = false,
+	separator = "%#SLSeparator#" .. icons.misc.RightThinBar .. " %*",
 }
 
 local encoding = {
@@ -137,7 +184,7 @@ local lsp = {
 
 		return "[" .. table.concat(buf_client_names, ", ") .. "]"
 	end,
-	icon = " LSP:",
+	icon = icons.ui.Cogs .. "LSP:",
 	color = { gui = "bold" },
 	cond = hide_in_width,
 }
@@ -158,11 +205,11 @@ local python_env = {
 		if vim.bo.filetype == "python" then
 			local venv = os.getenv("CONDA_DEFAULT_ENV")
 			if venv then
-				return string.format("  (%s)", python_env_cleanup(venv))
+				return string.format(" %s (%s)", icons.misc.Python, python_env_cleanup(venv))
 			end
 			venv = os.getenv("VIRTUAL_ENV")
 			if venv then
-				return string.format("  (%s)", python_env_cleanup(venv))
+				return string.format(" %s (%s)", icons.misc.Python, python_env_cleanup(venv))
 			end
 			return ""
 		end
@@ -176,7 +223,7 @@ local treesitter = {
 	function()
 		local b = vim.api.nvim_get_current_buf()
 		if next(vim.treesitter.highlighter.active[b]) then
-			return ""
+			return icons.misc.Tree
 		end
 		return ""
 	end,
@@ -194,11 +241,24 @@ local semantic_location = {
 	cond = hide_in_width and gps.is_available,
 }
 
+local location = {
+	"location",
+	color = "SLLocation",
+	separator = { left = " " },
+}
+
+local progress = {
+	"progress",
+	color = function()
+		return { fg = "#252525", bg = mode_color[vim.fn.mode()] }
+	end,
+}
+
 lualine.setup({
 	options = {
 		globalstatus = true,
 		icons_enabled = true,
-		theme = "auto",
+		theme = theme,
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard" },
@@ -206,12 +266,10 @@ lualine.setup({
 	},
 	sections = {
 		lualine_a = { mode, branch },
-		-- lualine_b = { branch, filename },
 		lualine_b = { diagnostics },
 		lualine_c = { python_env },
-		-- lualine_x = { diagnostics, treesitter, lsp, filetype, fileformat, spaces, encoding },
-		lualine_x = { diff, filetype, fileformat, spaces, encoding },
-		lualine_y = { "location" },
-		lualine_z = { "progress" },
+		lualine_x = { diff, spaces, encoding, fileformat, filetype },
+		lualine_y = { location },
+		lualine_z = { progress },
 	},
 })
