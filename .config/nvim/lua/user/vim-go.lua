597641445f79df617dfb9f81e0c9e6c20a706e8a
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-05 20:06:08 +0200

    Disable LSP capabilities of vim-go

--- .config/nvim/lua/user/vim-go.lua
+++ .config/nvim/lua/user/vim-go.lua
@@ -1,6 +1,11 @@
+vim.g.go_def_mapping_enabled = 0
+vim.g.go_fmt_autosave = 0
+vim.g.go_imports_autosave = 0
+vim.g.go_gopls_enabled = 0
+vim.g.go_diagnostics_level = 0
+vim.g.go_metalinter_enabled = 0
+
 local status_ok, _ = pcall(require, "vim-go")
 if not status_ok then
 	return
 end
-
-vim.g.go_def_mapping_enabled = 0
