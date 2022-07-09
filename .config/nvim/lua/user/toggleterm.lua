2a76e4368205099adb1a3c1fba3516fe9f87cf6e
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-09 10:14:47 +0200

    Change toggleterm mappings and lazygit config

--- .config/nvim/lua/user/toggleterm.lua
+++ .config/nvim/lua/user/toggleterm.lua
@@ -5,7 +5,7 @@ end
 
 toggleterm.setup({
 	size = 20,
-	open_mapping = [[<c-\>]],
+	open_mapping = [[<m-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
 	shading_factor = 2,
@@ -17,22 +17,39 @@ toggleterm.setup({
 	shell = vim.o.shell,
 	float_opts = {
 		border = "curved",
+		winblend = 0,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
 	},
 })
 
 function _G.set_terminal_keymaps()
 	local opts = { noremap = true }
 	-- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<m-h>", [[<C-\><C-n><C-W>h]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<m-j>", [[<C-\><C-n><C-W>j]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<m-k>", [[<C-\><C-n><C-W>k]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<m-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	hidden = true,
+	direction = "tab",
+	on_open = function(term)
+		vim.cmd("startinsert!")
+		vim.cmd("set laststatus=0")
+	end,
+	on_close = function(term)
+		vim.cmd("set laststatus=3")
+	end,
+})
 
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
