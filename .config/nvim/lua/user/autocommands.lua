97c5ad2ce3d0c1a3229acfde4a31be992e08c4cf
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-09 10:15:44 +0200

    Disable setting spell in gitcommit and markdown

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -27,12 +27,11 @@ vim.api.nvim_create_autocmd({ "User" }, {
 	end,
 })
 
--- Set wrap and spell in markdown and gitcommit
+-- Set wrap in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "gitcommit", "markdown" },
 	callback = function()
 		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
 	end,
 })
 
