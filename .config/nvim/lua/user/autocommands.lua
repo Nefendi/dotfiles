fbb803687876bf07ca916e8725ad99b4e0b4e235
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-12 20:00:57 +0200

    Use 'q' to quit in non-buffer types

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,3 +1,5 @@
+-- Source: https://github.com/ChristianChiarulli/nvim/blob/master/lua/user/autocommands.lua
+
 -- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir", "DressingSelect", "null-ls-info" },
@@ -6,6 +8,22 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
       nnoremap <silent> <buffer> q :close<CR> 
       set nobuflisted 
     ]])
+	end,
+})
+
+-- Use 'q' to quit in non-buffer types
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "" },
+	callback = function()
+		local buf_ft = vim.bo.filetype
+		if buf_ft == "" or buf_ft == nil then
+			vim.cmd([[
+      nnoremap <silent> <buffer> q :close<CR> 
+      " nnoremap <silent> <buffer> <c-j> j<CR> 
+      " nnoremap <silent> <buffer> <c-k> k<CR> 
+      set nobuflisted 
+    ]])
+		end
 	end,
 })
 
