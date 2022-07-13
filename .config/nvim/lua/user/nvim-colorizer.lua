91533b9de8e4651423b95ccafccfe492e6647887
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-13 20:08:21 +0200

    Fix colorizer's settings

--- .config/nvim/lua/user/nvim-colorizer.lua
+++ .config/nvim/lua/user/nvim-colorizer.lua
@@ -3,14 +3,15 @@ if not status_ok then
 	return
 end
 
-colorizer.setup({ "*" }, {
+colorizer.setup({ "*", css = { css = true } }, {
+	-- Settings for all modes
 	RGB = true, -- #RGB hex codes
 	RRGGBB = true, -- #RRGGBB hex codes
 	RRGGBBAA = true, -- #RRGGBBAA hex codes
 	rgb_fn = true, -- CSS rgb() and rgba() functions
 	hsl_fn = true, -- CSS hsl() and hsla() functions
-	css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-	css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 	names = false,
 	mode = "virtualtext",
 })
