ac1a74f911a9ea57139f708f36dc1c78a4ede400
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-14 22:35:36 +0200

    Turn off doc keyword setting in vim-go

--- .config/nvim/lua/user/vim-go.lua
+++ .config/nvim/lua/user/vim-go.lua
@@ -4,6 +4,7 @@ vim.g.go_imports_autosave = 0
 vim.g.go_gopls_enabled = 0
 vim.g.go_diagnostics_level = 0
 vim.g.go_metalinter_enabled = 0
+vim.g.go_doc_keywordprg_enabled = 0
 
 local status_ok, _ = pcall(require, "vim-go")
 if not status_ok then
