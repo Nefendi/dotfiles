d6f62ccdb0e35279713542297478c70c3571cf1c
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-14 16:59:17 +0200

    Add CursorHold pattern for winbar invocation

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -79,11 +79,14 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 
 -- Winbar
 if vim.fn.has("nvim-0.8") == 1 then
-	vim.api.nvim_create_autocmd({ "CursorMoved", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" }, {
-		callback = function()
-			require("user.winbar").get_winbar()
-		end,
-	})
+	vim.api.nvim_create_autocmd(
+		{ "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" },
+		{
+			callback = function()
+				require("user.winbar").get_winbar()
+			end,
+		}
+	)
 end
 
 -- Format on save
