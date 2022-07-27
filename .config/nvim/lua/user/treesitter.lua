784ba06de813576eab8ec2972986cdcbfa703579
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-27 17:54:07 +0200

    Disable matchup in treesitter

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -1,40 +1,40 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+    return
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-	},
-	autopairs = {
-		enable = true,
-	},
-	autotag = {
-		enable = true,
-		disable = { "markdown" },
-	},
-	indent = { enable = true, disable = { "python", "css", "markdown" } },
-	context_commentstring = { enable = true, disable = { "markdown" } },
-	rainbow = {
-		enable = true,
-		extended_mode = false,
-		max_file_lines = nil,
-		colors = {
-			-- "#bf616a",
-			-- "#d08770",
-			-- "#ebcb8b",
-			-- "#a3be8c",
-			-- "#88c0d0",
-			-- "#5e81ac",
-			-- "#b48ead",
-			"#68a0b0",
-			"#946EaD",
-			"#c7aA6D",
-		},
-		disable = { "html" },
-	},
-	playground = { enable = true },
+    ensure_installed = "all", -- one of "all" or a list of languages
+    ignore_install = { "" }, -- List of parsers to ignore installing
+    highlight = {
+        enable = true, -- false will disable the whole extension
+    },
+    autopairs = {
+        enable = true,
+    },
+    autotag = {
+        enable = true,
+        disable = { "markdown" },
+    },
+    indent = { enable = true, disable = { "python", "css", "markdown" } },
+    context_commentstring = { enable = true, disable = { "markdown" } },
+    rainbow = {
+        enable = true,
+        extended_mode = false,
+        max_file_lines = nil,
+        colors = {
+            -- "#bf616a",
+            -- "#d08770",
+            -- "#ebcb8b",
+            -- "#a3be8c",
+            -- "#88c0d0",
+            -- "#5e81ac",
+            -- "#b48ead",
+            "#68a0b0",
+            "#946EaD",
+            "#c7aA6D",
+        },
+        disable = { "html" },
+    },
+    playground = { enable = true },
 })
