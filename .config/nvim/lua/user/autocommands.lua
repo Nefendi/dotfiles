cdd7cfd22dd971880b522f663dde88d3f4d14469
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-29 16:14:51 +0200

    Tweak winbar's function

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -132,7 +132,10 @@ if vim.fn.has "nvim-0.8" == 1 then
         { "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" },
         {
             callback = function()
-                require("user.winbar").get_winbar()
+                local status_ok, _ = pcall(vim.api.nvim_buf_get_var, 0, "lsp_floating_window")
+                if not status_ok then
+                    require("user.winbar").get_winbar()
+                end
             end,
         }
     )
