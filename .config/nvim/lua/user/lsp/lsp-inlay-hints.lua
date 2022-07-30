a7b499423d0ecbffdf9c506ed3627f25951260eb
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-07-30 21:50:13 +0200

    Attach inlay hints without checking anything

--- .config/nvim/lua/user/lsp/lsp-inlay-hints.lua
+++ .config/nvim/lua/user/lsp/lsp-inlay-hints.lua
@@ -13,9 +13,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
         local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-        if client.server_capabilities.inlayHintProvider then
-            hints.on_attach(args.buf, client)
-        end
+        hints.on_attach(args.buf, client)
     end,
 })
 
