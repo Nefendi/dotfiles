735a105622be6b7294c94091c795f9be7b3bd0d3
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-08-01 16:40:02 +0200

    Modify onedarker italics settings for VictorMono

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,10 +1,10 @@
 local colorscheme = "onedarker"
 
-vim.g.onedarker_italic_keywords = true
+vim.g.onedarker_italic_keywords = false
 vim.g.onedarker_italic_functions = false
 vim.g.onedarker_italic_comments = true
-vim.g.onedarker_italic_loops = true
-vim.g.onedarker_italic_conditionals = true
+vim.g.onedarker_italic_loops = false
+vim.g.onedarker_italic_conditionals = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
