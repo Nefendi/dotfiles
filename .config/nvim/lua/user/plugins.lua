9120045321b6702c9cecf8663fcd48868fe80417
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-08-03 16:47:38 +0200

    Update Mason and inlay-hints plugins

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -119,9 +119,9 @@ return packer.startup(function(use)
 
     -- LSP
     use { "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" }
-    use { "williamboman/mason.nvim", commit = "2680ff415265665de7c88b766818e34870f522c3" }
-    use { "williamboman/mason-lspconfig.nvim", commit = "9dc0291fd9e3bdb56245f6bef95223cac9865231" }
-    use { "WhoIsSethDaniel/mason-tool-installer.nvim", commit = "3e9534a04e2850fa9bd107556c679b993a8b14e7" }
+    use { "williamboman/mason.nvim", commit = "ec59445db532f2e78eb254120b6774311179c155" }
+    use { "williamboman/mason-lspconfig.nvim", commit = "b0317c127e17feed26b2f7c0237d12f8a00de708" }
+    use { "WhoIsSethDaniel/mason-tool-installer.nvim", commit = "34402b49110bee675a49a2e9d23f4a477eb46c54" }
     use { "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" }
     use { "RRethy/vim-illuminate", commit = "c82e6d04f27a41d7fdcad9be0bce5bb59fcb78e5" }
     use { "folke/lsp-colors.nvim", commit = "517fe3ab6b63f9907b093bc9443ef06b56f804f3" }
@@ -133,7 +133,7 @@ return packer.startup(function(use)
         requires = { "neovim/nvim-lspconfig" },
     }
     use { "https://git.sr.ht/~whynothugo/lsp_lines.nvim", commit = "db67e94c813aae166c3d2f119ea7d2e85164922a" }
-    use { "lvimuser/lsp-inlayhints.nvim", commit = "aeba98288e4634d4112028187d51bda7fb9684fa" }
+    use { "lvimuser/lsp-inlayhints.nvim", commit = "33b5360cde2c280ee1f0618d413b07e7f6622c23" }
 
     -- Go
     use {
