4a92055d4c4089b8061572dde8e43c2a3ad1498d
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-09-24 13:53:32 +0200

    Tweak nvim-cmp tab mapping

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -8,6 +8,11 @@ if not snip_status_ok then
     return
 end
 
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local functions = require "user.functions"
@@ -71,12 +76,10 @@ cmp.setup {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expandable() then
-                luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif check_backspace() then
-                fallback()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
