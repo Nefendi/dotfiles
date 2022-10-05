30de9c4a65c476447c67b664257b4d415cc1e59b
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-10-05 18:06:54 +0200

    Reconfigure dapui and set debugger for Python

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -8,21 +8,22 @@ if not dap_ui_status_ok then
     return
 end
 
-local dap_install_status_ok, dap_install = pcall(require, "dap-install")
-if not dap_install_status_ok then
+local dap_python_status_ok, dap_python = pcall(require, "dap-python")
+if not dap_python_status_ok then
     return
 end
 
 local icons = require "user.icons"
 
-dap_install.setup {}
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
 
-dap_install.config("python", {})
+-- Python
+dap_python.setup(mason_path .. "packages/debugpy/venv/bin/python")
+dap_python.test_runner = "pytest"
 
 dapui.setup {
     icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
     mappings = {
-        -- Use a table to apply multiple mappings
         expand = { "<CR>", "<2-LeftMouse>" },
         open = "o",
         remove = "d",
@@ -30,65 +31,34 @@ dapui.setup {
         repl = "r",
         toggle = "t",
     },
-    -- Expand lines larger than the window
-    -- Requires >= 0.7
     expand_lines = vim.fn.has "nvim-0.7",
-    -- Layouts define sections of the screen to place windows.
-    -- The position can be "left", "right", "top" or "bottom".
-    -- The size specifies the height/width depending on position. It can be an Int
-    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-    -- Elements are the elements shown in the layout (in order).
-    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
     layouts = {
         {
             elements = {
-                -- Elements can be strings or table with id and size keys.
-                { id = "scopes", size = 0.25 },
-                "breakpoints",
-                -- "stacks",
-                -- "watches",
+                { id = "scopes", size = 0.33 },
+                { id = "breakpoints", size = 0.17 },
+                { id = "stacks", size = 0.25 },
+                { id = "watches", size = 0.25 },
             },
-            size = 40, -- 40 columns
+            size = 0.33,
             position = "right",
         },
         {
             elements = {
-                "repl",
-                "console",
+                { id = "repl", size = 0.45 },
+                { id = "console", size = 0.55 },
             },
-            size = 0.25, -- 25% of total lines
+            size = 0.27,
             position = "bottom",
         },
     },
-    controls = {
-        -- Requires Neovim nightly (or 0.8 when released)
-        enabled = true,
-        -- Display controls in this element
-        element = "repl",
-        icons = {
-            pause = "",
-            play = "",
-            step_into = "",
-            step_over = "",
-            step_out = "",
-            step_back = "",
-            run_last = "↻",
-            terminate = "□",
-        },
-    },
     floating = {
-        max_height = nil, -- These can be integers or a float between 0 and 1.
-        max_width = nil, -- Floats will be treated as percentage of your screen.
-        border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+        max_height = 0.9,
+        max_width = 0.5,
+        border = "rounded",
         mappings = {
             close = { "q", "<Esc>" },
         },
-    },
-    windows = { indent = 1 },
-    render = {
-        max_type_length = nil, -- Can be integer or nil.
-        max_value_lines = 100, -- Can be integer or nil.
     },
 }
 
@@ -96,6 +66,18 @@ vim.fn.sign_define(
     "DapBreakpoint",
     { text = icons.misc.SmallDot, texthl = "DiagnosticSignError", linehl = "", numhl = "" }
 )
+
+vim.fn.sign_define(
+    "DapBreakpointRejected",
+    { text = icons.misc.SmallDot, texthl = "DiagnosticSignHint", linehl = "", numhl = "" }
+)
+
+vim.fn.sign_define("DapStopped", {
+    text = icons.ui.BoldArrowRight,
+    texthl = "DiagnosticSignInfo",
+    linehl = "DiagnosticUnderlineInfo",
+    numhl = "DiagnosticSignInfo",
+})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
