dc8ff677c4f782cff7114c83615bfc7410283830
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-10-05 18:07:14 +0200

    Add Python-specific ftplugin file

---
+++ .config/nvim/ftplugin/python.lua
@@ -0,0 +1,39 @@
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+    return
+end
+
+local opts = {
+    mode = "n",
+    prefix = "<leader>",
+    buffer = nil,
+    silent = true,
+    noremap = true,
+    nowait = true,
+}
+
+local vopts = {
+    mode = "v",
+    prefix = "<leader>",
+    buffer = nil,
+    silent = true,
+    noremap = true,
+    nowait = true,
+}
+
+local mappings = {
+    d = {
+        m = { "<cmd>lua require('dap-python').test_method()<cr>", "Test Method" },
+        f = { "<cmd>lua require('dap-python').test_class()<cr>", "Test Class" },
+    },
+}
+
+local vmappings = {
+    d = {
+        name = "Debug",
+        s = { "<esc><cmd>lua require('dap-python').debug_selection()<cr>", "Debug Selection" },
+    },
+}
+
+which_key.register(mappings, opts)
+which_key.register(vmappings, vopts)
