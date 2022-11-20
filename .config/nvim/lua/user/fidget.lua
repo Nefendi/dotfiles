35bd1496bff23f934c1934ae8780428aa89cd1a1
Nefendi <28621696+Nefendi@users.noreply.github.com>
2022-11-20 12:58:25 +0100

    Change the colour of FidgetTask HL group in fidget

--- .config/nvim/lua/user/fidget.lua
+++ .config/nvim/lua/user/fidget.lua
@@ -8,3 +8,7 @@ fidget.setup {
         spinner = "dots",
     },
 }
+
+local comment_hl_group = vim.api.nvim_get_hl_by_name("Comment", true)
+
+vim.api.nvim_set_hl(0, "FidgetTask", { fg = comment_hl_group.foreground })
