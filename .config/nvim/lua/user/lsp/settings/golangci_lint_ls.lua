ef4eb22343e52239d3058894d73d9ad27776dce3
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-02-13 22:34:06 +0100

    Do not enable almost all checks in golangci-lint

--- .config/nvim/lua/user/lsp/settings/golangci_lint_ls.lua
+++ .config/nvim/lua/user/lsp/settings/golangci_lint_ls.lua
@@ -4,9 +4,6 @@ return {
         command = {
             "golangci-lint",
             "run",
-            "--enable-all",
-            "--disable",
-            "lll",
             "--out-format",
             "json",
             "--issues-exit-code=1",
