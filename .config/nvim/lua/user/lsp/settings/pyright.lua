2e70894dc9843c0b2ccc67f42765d87a113526eb
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-04-06 12:48:44 +0200

    Enable autoSearchPaths in Pyright
    Not sure what it really does :|

--- .config/nvim/lua/user/lsp/settings/pyright.lua
+++ .config/nvim/lua/user/lsp/settings/pyright.lua
@@ -6,6 +6,7 @@ return {
                 diagnosticMode = "workspace",
                 useLibraryCodeForTypes = true,
                 autoImportCompletions = true,
+                autoSearchPaths = true,
             },
         },
     },
