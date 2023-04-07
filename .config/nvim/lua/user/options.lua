24618f1dc0970703fa20b0b492febd4e48779604
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-04-07 15:26:51 +0200

    Add comment about cmdheight and noice.nvim

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,5 +1,6 @@
 vim.opt.backup = false -- creates a backup file
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+-- noice.nvim hijacks cmdheight and sets it always to 0
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
