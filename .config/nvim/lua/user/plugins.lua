9ea26cbcc6c390522540b20c9ebae4fc950e22ad
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-04-07 20:33:43 +0200

    Load every plugins lazily be default

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -15,20 +15,20 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Install your plugins here
 require("lazy").setup({
-    { "nvim-lua/plenary.nvim", lazy = true }, -- Useful lua functions used by lots of plugins
+    { "nvim-lua/plenary.nvim" }, -- Useful lua functions used by lots of plugins
     { "windwp/nvim-autopairs" }, -- Autopairs, integrates with both cmp and treesitter
-    { "numToStr/Comment.nvim", lazy = true },
+    { "numToStr/Comment.nvim" },
     { "kyazdani42/nvim-tree.lua" },
-    { "nvim-tree/nvim-web-devicons", lazy = true },
+    { "nvim-tree/nvim-web-devicons" },
     { "moll/vim-bbye" },
     { "nvim-lualine/lualine.nvim" },
     { "akinsho/toggleterm.nvim", version = "v2.*" },
     { "ahmedkhalf/project.nvim", event = "VimEnter" },
-    { "lukas-reineke/indent-blankline.nvim", lazy = true },
+    { "lukas-reineke/indent-blankline.nvim" },
     { "goolord/alpha-nvim" },
     { "kylechui/nvim-surround" },
     { "tpope/vim-repeat" },
-    { "lervag/vimtex", lazy = true },
+    { "lervag/vimtex" },
     { "folke/todo-comments.nvim" },
     { "ggandor/leap.nvim" },
     { "ggandor/flit.nvim" },
@@ -37,8 +37,8 @@ require("lazy").setup({
     { "ellisonleao/glow.nvim" },
     { "rcarriga/nvim-notify" },
     { "folke/which-key.nvim", event = "VeryLazy" },
-    { "nvim-pack/nvim-spectre", lazy = true },
-    { "stevearc/dressing.nvim", lazy = true },
+    { "nvim-pack/nvim-spectre" },
+    { "stevearc/dressing.nvim" },
     { "abecodes/tabout.nvim" },
     { "ghillb/cybu.nvim" },
     { "karb94/neoscroll.nvim" },
@@ -66,24 +66,22 @@ require("lazy").setup({
         commit = "733b4dd2c1b87013291c21d80afbe5255712d1d3",
     },
     { "tamago324/lir.nvim" },
-    { "b0o/schemastore.nvim", lazy = true, lazy = true },
+    { "b0o/schemastore.nvim" },
     {
         "danymat/neogen",
-        lazy = true,
         dependencies = "nvim-treesitter/nvim-treesitter",
         commit = "967b280d7d7ade52d97d06e868ec4d9a0bc59282",
     },
-    { "monaqa/dial.nvim", lazy = true },
+    { "monaqa/dial.nvim" },
     {
         "utilyre/barbecue.nvim",
-        lazy = true,
         dependencies = {
             "SmiteshP/nvim-navic",
             "nvim-tree/nvim-web-devicons",
         },
         commit = "a0e19486ccc1fb134f6ef7bf914d07bbb462e346",
     },
-    { "folke/neodev.nvim", lazy = true },
+    { "folke/neodev.nvim" },
     {
         "kosayoda/nvim-lightbulb",
         dependencies = "antoinemadec/FixCursorHold.nvim",
@@ -91,7 +89,6 @@ require("lazy").setup({
     },
     {
         "folke/noice.nvim",
-        lazy = true,
         dependencies = {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify",
@@ -99,22 +96,21 @@ require("lazy").setup({
     },
 
     -- Colorschemes
-    { "lunarvim/onedarker.nvim", lazy = true },
-    { "lunarvim/darkplus.nvim", lazy = true },
+    { "lunarvim/onedarker.nvim" },
+    { "lunarvim/darkplus.nvim" },
     { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
     { "fladson/vim-kitty" },
 
     -- Cmp plugins
-    { "hrsh7th/nvim-cmp", lazy = true }, -- The completion plugin
-    { "hrsh7th/cmp-buffer", lazy = true }, -- buffer completions
-    { "hrsh7th/cmp-path", lazy = true }, -- path completions
-    { "saadparwaiz1/cmp_luasnip", lazy = true }, -- snippet completions
-    { "hrsh7th/cmp-nvim-lsp", lazy = true },
-    { "hrsh7th/cmp-nvim-lua", lazy = true },
-    { "hrsh7th/cmp-emoji", lazy = true },
+    { "hrsh7th/nvim-cmp" }, -- The completion plugin
+    { "hrsh7th/cmp-buffer" }, -- buffer completions
+    { "hrsh7th/cmp-path" }, -- path completions
+    { "saadparwaiz1/cmp_luasnip" }, -- snippet completions
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-nvim-lua" },
+    { "hrsh7th/cmp-emoji" },
     {
         "Saecki/crates.nvim",
-        lazy = true,
         version = "v0.3.0",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
@@ -125,29 +121,27 @@ require("lazy").setup({
     },
     {
         "tzachar/cmp-tabnine",
-        lazy = true,
         commit = "a6cb553143573861d2d98da86ceb3074c87fc536",
         dependencies = { "hrsh7th/nvim-cmp" },
         build = "./install.sh",
     },
 
     -- Snippets
-    { "L3MON4D3/LuaSnip", lazy = true },
-    { "rafamadriz/friendly-snippets", lazy = true },
+    { "L3MON4D3/LuaSnip" },
+    { "rafamadriz/friendly-snippets" },
 
     -- LSP
-    { "neovim/nvim-lspconfig", lazy = true },
-    { "williamboman/mason.nvim", lazy = true },
-    { "williamboman/mason-lspconfig.nvim", lazy = true },
-    { "WhoIsSethDaniel/mason-tool-installer.nvim", lazy = true },
-    { "jose-elias-alvarez/null-ls.nvim", lazy = true },
-    { "RRethy/vim-illuminate", lazy = true },
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    { "jose-elias-alvarez/null-ls.nvim" },
+    { "RRethy/vim-illuminate" },
     { "folke/lsp-colors.nvim" },
     { "ray-x/lsp_signature.nvim" },
-    { "smjonas/inc-rename.nvim", lazy = true },
+    { "smjonas/inc-rename.nvim" },
     {
         "SmiteshP/nvim-navic",
-        lazy = true,
         commit = "7e9d2b2b601149fecdccd11b516acb721e571fe6",
         dependencies = { "neovim/nvim-lspconfig" },
     },
@@ -182,19 +176,17 @@ require("lazy").setup({
     { "mfussenegger/nvim-jdtls" },
 
     -- Telescope
-    { "nvim-telescope/telescope.nvim", lazy = true, branch = "0.1.x" },
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
     {
         "nvim-telescope/telescope-fzf-native.nvim",
-        lazy = true,
         commit = "6a33ecefa9b3d9ade654f9a7a6396a00c3758ca6",
         build = "make",
     },
-    { "nvim-telescope/telescope-media-files.nvim", lazy = true },
+    { "nvim-telescope/telescope-media-files.nvim" },
 
     -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
-        lazy = true,
         commit = "89e5fa66cf53854f45cfcfae45afb93171cf5c05",
         build = function()
             require("nvim-treesitter.install").update { with_sync = true }
@@ -202,21 +194,21 @@ require("lazy").setup({
     },
     -- { "p00f/nvim-ts-rainbow", lazy = false },
     { "mrjones2014/nvim-ts-rainbow", lazy = false },
-    { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
-    { "windwp/nvim-ts-autotag", lazy = true },
-    { "nvim-treesitter/playground", lazy = true },
-    { "nvim-treesitter/nvim-treesitter-context", lazy = true },
-    { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
+    { "JoosepAlviste/nvim-ts-context-commentstring" },
+    { "windwp/nvim-ts-autotag" },
+    { "nvim-treesitter/playground" },
+    { "nvim-treesitter/nvim-treesitter-context" },
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
 
     -- Git
-    { "lewis6991/gitsigns.nvim", lazy = true },
-    { "akinsho/git-conflict.nvim", lazy = true },
+    { "lewis6991/gitsigns.nvim" },
+    { "akinsho/git-conflict.nvim" },
 
     -- DAP
-    { "mfussenegger/nvim-dap", lazy = true },
-    { "mfussenegger/nvim-dap-python", lazy = true },
-    { "leoluz/nvim-dap-go", lazy = true },
-    { "rcarriga/nvim-dap-ui", lazy = true },
+    { "mfussenegger/nvim-dap" },
+    { "mfussenegger/nvim-dap-python" },
+    { "leoluz/nvim-dap-go" },
+    { "rcarriga/nvim-dap-ui" },
 
     -- nvim-test
     --  { "klen/nvim-test" },
@@ -224,7 +216,6 @@ require("lazy").setup({
     -- Neotest
     {
         "nvim-neotest/neotest",
-        lazy = true,
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
@@ -232,13 +223,16 @@ require("lazy").setup({
         },
         commit = "568feb5e9c7c5b7386886ecd4520389153aa7c7d",
     },
-    { "rouge8/neotest-rust", lazy = true },
-    { "nvim-neotest/neotest-python", lazy = true },
-    { "nvim-neotest/neotest-go", lazy = true },
-    { "haydenmeade/neotest-jest", lazy = true },
-    { "marilari88/neotest-vitest", lazy = true },
+    { "rouge8/neotest-rust" },
+    { "nvim-neotest/neotest-python" },
+    { "nvim-neotest/neotest-go" },
+    { "haydenmeade/neotest-jest" },
+    { "marilari88/neotest-vitest" },
 }, {
     ui = {
         border = "rounded",
+    },
+    defaults = {
+        lazy = true,
     },
 })
