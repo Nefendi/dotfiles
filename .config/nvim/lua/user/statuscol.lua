7789f5d15d08550fcd048d9ecf33a22ac4bc02fe
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-04-08 20:24:39 +0200

    Update plugins

--- .config/nvim/lua/user/statuscol.lua
+++ .config/nvim/lua/user/statuscol.lua
@@ -3,10 +3,22 @@ if not status_ok then
     return
 end
 
+local builtin = require "statuscol.builtin"
+
 statuscol.setup {
-    foldfunc = "builtin",
     setopt = true,
-    order = "SsNsFs",
+    segments = {
+        -- Sign + " "
+        { text = { "%s", " " }, click = "v:lua.ScSa" },
+        -- Line number + " "
+        {
+            text = { builtin.lnumfunc, " " },
+            condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+        },
+        -- Foldcolumn + " "
+        { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+    },
     ft_ignore = {
         "help",
         "spectre_panel",
