ed24459d49d0899ae4abc1ec3b56a922f135feef
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-05-14 17:34:34 +0200

    Remove unnecessary keymaps in noice

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -151,17 +151,4 @@ keymap("n", "-", ":lua require'lir.float'.toggle()<cr>", opts)
 -- Formatting
 -- keymap("n", "<leader>F", "<cmd>LspToggleAutoFormat<cr>", opts)
 
--- Noice
-vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
-    if not require("noice.lsp").scroll(4) then
-        return "<c-f>"
-    end
-end, { silent = true, expr = true })
-
-vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
-    if not require("noice.lsp").scroll(-4) then
-        return "<c-b>"
-    end
-end, { silent = true, expr = true })
-
 return M
