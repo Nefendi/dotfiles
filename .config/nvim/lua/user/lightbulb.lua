974476b63203d8ddfdf25720929b59075fa938e8
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-07-08 12:19:53 +0200

    Adjust lightbulb's configuration to the newest version

--- .config/nvim/lua/user/lightbulb.lua
+++ .config/nvim/lua/user/lightbulb.lua
@@ -6,20 +6,17 @@ end
 local icons = require "user.icons"
 
 lightbulb.setup {
-    sign = { enabled = true },
+    sign = { enabled = true, text = icons.diagnostics.Hint },
+    number = { enabled = false, text = icons.diagnostics.Hint },
     autocmd = { enabled = true },
     float = {
         enabled = false,
         text = icons.diagnostics.Hint,
-        win_opts = { anchor = "SE", border = "none", style = "minimal" },
     },
     virtual_text = { enabled = false, text = icons.diagnostics.Hint, hl_mode = "replace" },
 }
 
-vim.fn.sign_define(
-    "LightBulbSign",
-    { text = icons.diagnostics.Hint, texthl = "CmpItemKindEmoji", linehl = "", numhl = "" }
-)
-
 vim.api.nvim_set_hl(0, "LightBulbFloatWin", { fg = "#fde030", bg = "#1a1b26" })
 vim.api.nvim_set_hl(0, "LightBulbVirtualText", { fg = "#fde030", bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "LightBulbSign", { fg = "#fde030", bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "LightBulbNumber", { fg = "#fde030", bg = "#1a1b26" })
