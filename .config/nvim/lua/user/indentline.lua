6bd8c5d5edf541dda2c32a40295b25537c310a10
Nefendi <28621696+Nefendi@users.noreply.github.com>
2023-10-11 21:54:16 +0200

    Adjust indent-blankline config to the newest version

--- .config/nvim/lua/user/indentline.lua
+++ .config/nvim/lua/user/indentline.lua
@@ -5,8 +5,6 @@ end
 
 local hooks = require "ibl.hooks"
 
-local icons = require "user.icons"
-
 local highlights = {
     "RainbowDelimiterRed",
     "RainbowDelimiterYellow",
@@ -17,52 +15,31 @@ local highlights = {
     "RainbowDelimiterCyan",
 }
 
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-    "help",
-    "startify",
-    "dashboard",
-    "packer",
-    "neogitstatus",
-    "NvimTree",
-    "Trouble",
-    "text",
-}
-
-vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_char = icons.misc.LeftThinBar
--- vim.g.indent_blankline_char_list = { "|", "¦", "┆", "┊" }
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_use_treesitter = false
-vim.g.indent_blankline_show_current_context = true
-
--- vim.g.indent_blankline_context_patterns = {
---     "class",
---     "return",
---     "function",
---     "method",
---     "^if",
---     "^while",
---     "jsx_element",
---     "^for",
---     "^object",
---     "^table",
---     "block",
---     "arguments",
---     "if_statement",
---     "else_clause",
---     "jsx_element",
---     "jsx_self_closing_element",
---     "try_statement",
---     "catch_clause",
---     "import_statement",
---     "operation_type",
--- }
+local indent_chars = { "╎", "╏", "┆", "┇", "┊", "┋" }
 
 indent_blankline.setup {
-    show_current_context = true,
-    scope = { highlight = highlights },
+    scope = {
+        char = indent_chars,
+        highlight = highlights,
+        show_start = false,
+        show_end = false,
+    },
+    indent = {
+        char = indent_chars,
+    },
+    exclude = {
+        filetypes = {
+            "help",
+            "startify",
+            "dashboard",
+            "packer",
+            "neogitstatus",
+            "NvimTree",
+            "Trouble",
+            "text",
+        },
+        buftypes = { "terminal", "nofile" },
+    },
 }
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
