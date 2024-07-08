9996a8f95618a536ad53135faee79dea229789a5
Nefendi <nefendi.2snfj@slmail.me>
2024-07-08 20:41:27 +0200

    Add compositeLiteralTypes inlay hints in gopls

--- .config/nvim/lua/user/lsp/settings/gopls.lua
+++ .config/nvim/lua/user/lsp/settings/gopls.lua
@@ -4,6 +4,7 @@ return {
             hints = {
                 assignVariableTypes = true,
                 compositeLiteralFields = true,
+                compositeLiteralTypes = true,
                 constantValues = true,
                 functionTypeParameters = true,
                 parameterNames = true,
