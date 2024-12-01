0fc3f11704f9e0adecdbfece86b06b1b9c037fc5
Nefendi <nefendi.2snfj@slmail.me>
2024-12-01 09:11:45 +0100

    Remove proto from clangd filetypes

--- .config/nvim/lua/user/lsp/settings/clangd.lua
+++ .config/nvim/lua/user/lsp/settings/clangd.lua
@@ -2,4 +2,13 @@ return {
     -- This is needed to overcome the issue with multiple offset encodings:
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
     capabilities = { offsetEncoding = { "utf-16" } },
+    filetypes = {
+        "c",
+        "cpp",
+        "objc",
+        "objcpp",
+        "cuda",
+        -- Removed because of protobuf files
+        -- "proto"
+    },
 }
