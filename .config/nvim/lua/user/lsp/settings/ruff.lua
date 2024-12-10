3340115f4980e370a0a52f7c6825ab383874cd02
Nefendi <nefendi.2snfj@slmail.me>
2024-12-10 15:05:05 +0100

    Do not use report feature in preview in ruff lint

--- .config/nvim/lua/user/lsp/settings/ruff.lua
+++ .config/nvim/lua/user/lsp/settings/ruff.lua
@@ -4,7 +4,7 @@ return {
     init_options = {
         settings = {
             lint = {
-                preview = true,
+                preview = false,
             },
             format = {
                 preview = false,
