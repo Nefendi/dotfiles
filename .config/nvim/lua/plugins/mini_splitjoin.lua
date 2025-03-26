9945ff57d80e80f84681f4afde02fc5f45c5a392
Nefendi <nefendi.2snfj@slmail.me>
2025-03-26 16:05:49 +0100

    Add mini.splitjoin plugin

---
+++ .config/nvim/lua/plugins/mini_splitjoin.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "echasnovski/mini.splitjoin",
+        version = false,
+
+        config = function()
+            local mini_splitjoin = require "mini.splitjoin"
+
+            mini_splitjoin.setup {}
+        end,
+    },
+}
