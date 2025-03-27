82a1fbff4a39ffcfa9b2befd92319ad50c896b46
Nefendi <nefendi.2snfj@slmail.me>
2025-03-27 20:52:07 +0100

    Reorganize neotest's config

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -12,6 +12,7 @@ return {
             "marilari88/neotest-vitest",
             "jfpedroza/neotest-elixir",
             "lawrence-laz/neotest-zig",
+            "mrcjkb/rustaceanvim",
             -- FIXME: The config.lsp file needs to be run before rustaceanvim neotest can be configured here.
             -- Try to find a better way to do that in the future.
             "neovim/nvim-lspconfig",
@@ -31,31 +32,17 @@ return {
             },
         },
         config = function(_, opts)
-            local neotest = require "neotest"
-
-            local neotest_python = require "neotest-python"
-
-            local neotest_go = require "neotest-go"
-
-            local neotest_jest = require "neotest-jest"
-
-            local neotest_vitest = require "neotest-vitest"
-
-            local neotest_elixir = require "neotest-elixir"
-
-            local neotest_zig = require "neotest-zig"
-
             opts.adapters = {
                 require "rustaceanvim.neotest",
-                neotest_python,
-                neotest_go,
-                neotest_jest,
-                neotest_vitest,
-                neotest_elixir,
-                neotest_zig,
+                require "neotest-python",
+                require "neotest-go",
+                require "neotest-jest",
+                require "neotest-vitest",
+                require "neotest-elixir",
+                require "neotest-zig",
             }
 
-            neotest.setup(opts)
+            require("neotest").setup(opts)
         end,
     },
 }
