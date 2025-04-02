6622811641cfb51c3a39ac1e3d618dc101acbe1b
Nefendi <nefendi.2snfj@slmail.me>
2025-04-02 22:08:45 +0200

    Reorganize treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,120 +1,78 @@
+-- Taken from https://www.lazyvim.org/plugins/treesitter
+
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        version = false,
         build = ":TSUpdate",
+        event = "VeryLazy",
+        lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
         dependencies = {
             { "HiPhish/rainbow-delimiters.nvim", event = "BufReadPre" },
             { "JoosepAlviste/nvim-ts-context-commentstring" },
-            { "windwp/nvim-ts-autotag", event = "BufReadPost" },
             { "nvim-treesitter/nvim-treesitter-context", event = "BufReadPost" },
             { "nvim-treesitter/nvim-treesitter-textobjects", event = "BufReadPost" },
             { "RRethy/nvim-treesitter-endwise", event = "BufReadPost" },
             "IndianBoy42/tree-sitter-just",
             "vrischmann/tree-sitter-templ",
         },
-        config = function()
-            local configs = require "nvim-treesitter.configs"
-
+        init = function(plugin)
+            -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+            -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+            -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+            -- Luckily, the only things that those plugins need are the custom queries, which we make available
+            -- during startup.
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require "nvim-treesitter.query_predicates"
+        end,
+        opts_extend = { "ensure_installed" },
+        opts = {
+            ensure_installed = "all",
+            highlight = { enable = true },
+            indent = { enable = true },
+            autopairs = { enable = true },
+            autotag = {
+                enable = true,
+                disable = { "markdown" },
+            },
+            endwise = {
+                enable = true,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_start = {
+                        ["]f"] = "@function.outer",
+                        ["]c"] = "@class.outer",
+                        ["]a"] = "@parameter.inner",
+                    },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+                    goto_previous_start = {
+                        ["[f"] = "@function.outer",
+                        ["[c"] = "@class.outer",
+                        ["[a"] = "@parameter.inner",
+                    },
+                    goto_previous_end = {
+                        ["[F"] = "@function.outer",
+                        ["[C"] = "@class.outer",
+                        ["[A"] = "@parameter.inner",
+                    },
+                },
+            },
+        },
+        config = function(_, opts)
             require("nvim-dap-repl-highlights").setup()
             require("tree-sitter-just").setup {}
-
-            configs.setup {
-                ensure_installed = "all", -- one of "all" or a list of languages
-                ignore_install = { "" }, -- List of parsers to ignore installing
-                highlight = {
-                    enable = true, -- false will disable the whole extension
-                },
-                autopairs = { enable = true },
-                autotag = {
-                    enable = true,
-                    disable = { "markdown" },
-                },
-                endwise = {
-                    enable = true,
-                },
-                indent = { enable = true, disable = { "python", "css", "markdown" } },
-                playground = { enable = true },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["at"] = "@call.outer",
-                            ["it"] = "@call.inner",
-                            ["ap"] = "@parameter.outer",
-                            ["ip"] = "@parameter.inner",
-                            ["al"] = "@loop.outer",
-                            ["il"] = "@loop.inner",
-                            ["ai"] = "@conditional.outer",
-                            ["ii"] = "@conditional.inner",
-                            ["a/"] = "@comment.outer",
-                            ["i/"] = "@comment.inner",
-                            ["ab"] = "@block.outer",
-                            ["ib"] = "@block.inner",
-                            ["as"] = "@statement.outer",
-                            ["aa"] = "@attribute.outer",
-                            ["ia"] = "@attribute.inner",
-                            ["aF"] = "@frame.outer",
-                            ["iF"] = "@frame.inner",
-                            ["ar"] = "@regex.outer",
-                            ["ir"] = "@regex.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["<leader>;"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["<leader>'"] = "@parameter.inner",
-                        },
-                    },
-                },
-            }
-
-            -- nvim-treesitter-context
-
-            -- Nord palette
-            -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#3B4252" })
-            -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = "#D8DEE9" })
-
-            -- Darkplus palette
-            -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = "#D4D4D4" })
-
-            -- Onedarker palette
-            -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = "#ABB2BF" })
-
-            require("treesitter-context").setup {
-                enable = true,
-                max_lines = 0,
-                min_window_height = 40, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-            }
 
             local rainbow_delimiters = require "rainbow-delimiters"
 
@@ -139,12 +97,7 @@ return {
                 blacklist = { "html" },
             }
 
-            vim.g.skip_ts_context_commentstring_module = true
-
-            require("ts_context_commentstring").setup {
-                enable_autocmd = false,
-                disable = { "markdown" },
-            }
+            require("nvim-treesitter.configs").setup(opts)
         end,
     },
 }
