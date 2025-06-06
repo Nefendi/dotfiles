3f041bce6e9355b08d672e1b7a732c09c47be57b
Nefendi <nefendi.2snfj@slmail.me>
2025-06-06 21:36:04 +0200

    Enable and add a mapping for snacks zen

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -17,6 +17,7 @@ return {
                 pattern = "VeryLazy",
                 callback = function()
                     Snacks.toggle.dim():map "<leader>D"
+                    Snacks.toggle.zen():map "<leader>z"
                 end,
             })
         end,
@@ -273,6 +274,7 @@ return {
             undo = { enabled = true },
             bigfile = { enabled = true },
             gitbrowse = { enabled = true },
+            zen = { enabled = true },
             picker = {
                 enabled = true,
                 exclude = {
