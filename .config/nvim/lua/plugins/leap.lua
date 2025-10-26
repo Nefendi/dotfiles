bde0928fc41471b590ea3cb97709b4856d6826a1
Nefendi <nefendi.2snfj@slmail.me>
2025-10-26 16:06:29 +0100

    Update plugins

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -19,7 +19,8 @@ return {
 
             vim.api.nvim_set_hl(0, "LeapBackdrop", { fg = "#707070" })
 
-            leap.set_default_mappings()
+            vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+            vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
 
             local clever = require("leap.user").with_traversal_keys
             local clever_f = clever("f", "F")
