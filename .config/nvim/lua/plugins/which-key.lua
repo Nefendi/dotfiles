23fe48c029080a264ce42a834f4fda2f350268f3
Nefendi <nefendi.2snfj@slmail.me>
2025-12-22 18:33:22 +0100

    Make which-key's config shorter

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,57 +2,24 @@ return {
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
-        config = function()
+        opts = {
+            preset = "modern",
+            win = {
+                padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+                wo = {
+                    winblend = 0,
+                },
+            },
+            layout = {
+                height = { min = 4, max = 25 }, -- min and max height of the columns
+                width = { min = 20, max = 50 }, -- min and max width of the columns
+            },
+        },
+
+        config = function(_, opts)
             local which_key = require "which-key"
 
-            local icons = require "config.icons"
-
             local neotest = require "neotest"
-
-            local setup = {
-                preset = "modern",
-                plugins = {
-                    marks = true, -- shows a list of your marks on ' and `
-                    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-                    spelling = {
-                        enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-                        suggestions = 20, -- how many suggestions should be shown in the list?
-                    }, -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-                    -- No actual key bindings are created
-                    presets = {
-                        operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-                        motions = false, -- adds help for motions
-                        text_objects = true, -- help for text objects triggered after entering an operator
-                        windows = true, -- default bindings on <c-w>
-                        nav = true, -- misc bindings to work with windows
-                        z = true, -- bindings for folds, spelling and others prefixed with z
-                        g = true, -- bindings for prefixed with g
-                    },
-                },
-                icons = {
-                    breadcrumb = icons.ui.DoubleRightChevron, -- symbol used in the command line area that shows your active key combo
-                    separator = icons.ui.RightArrow, -- symbol used between a key and it's label
-                    group = "+", -- symbol prepended to a group
-                },
-                keys = {
-                    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-                    scroll_up = "<c-u>", -- binding to scroll up inside the popup
-                },
-                win = {
-                    border = "rounded", -- none, single, double, shadow
-                    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-                    wo = {
-                        winblend = 0,
-                    },
-                },
-                layout = {
-                    height = { min = 4, max = 25 }, -- min and max height of the columns
-                    width = { min = 20, max = 50 }, -- min and max width of the columns
-                    spacing = 3, -- spacing between columns
-                    align = "center", -- align columns left, center or right
-                },
-                show_help = false, -- show help message on the command line when the popup is visible
-            }
 
             local mappings = {
                 { "<leader>T", group = "Treesitter", nowait = true, remap = false },
@@ -439,7 +406,7 @@ return {
                 },
             }
 
-            which_key.setup(setup)
+            which_key.setup(opts)
             which_key.add(mappings)
             which_key.add(vmappings)
         end,
