0fffa8143f96070cb461a2d77103fc0878c4341a
Nefendi <nefendi.2snfj@slmail.me>
2026-01-13 15:13:04 +0100

    Disable drag_and_drop in img-clip to get rid of the annoying "content is not an image" warning

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -91,7 +91,7 @@ return {
                     embed_image_as_base64 = false,
                     prompt_for_file_name = false,
                     drag_and_drop = {
-                        insert_mode = true,
+                        insert_mode = false,
                     },
                     -- required for Windows users
                     use_absolute_path = true,
