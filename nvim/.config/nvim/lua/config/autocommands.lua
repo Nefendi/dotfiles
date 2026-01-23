67dba5d3b75279d953386b95cf9c180207730497
Nefendi <nefendi.2snfj@slmail.me>
2026-01-23 17:37:26 +0100

    Fix ctrl+l in neovim

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -120,3 +120,26 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
         vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
     end,
 })
+
+-- Tell kitty that we are inside an editor
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume", "UIEnter" }, {
+    group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+    callback = function()
+        if vim.api.nvim_ui_send then
+            vim.api.nvim_ui_send "\x1b]1337;SetUserVar=in_editor=MQo\007"
+        else
+            io.stdout:write "\x1b]1337;SetUserVar=in_editor=MQo\007"
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+    group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
+    callback = function()
+        if vim.api.nvim_ui_send then
+            vim.api.nvim_ui_send "\x1b]1337;SetUserVar=in_editor=MQo\007"
+        else
+            io.stdout:write "\x1b]1337;SetUserVar=in_editor\007"
+        end
+    end,
+})
